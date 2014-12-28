fe87de623d6a8fa1428429a9105fd162aa6e6573
jpbatz <jph@hawaii.edu>
2014-12-27 20:00:26 -1000

    MDA adds account routes and functionality

--- app/server.js
+++ app/server.js
@@ -78,7 +78,9 @@ passport.serializeUser(function(user, done) {
 });
 
 passport.deserializeUser(function(user, done) {
-  done(null, user);
+  User.findById(user._id, function (err, user) {
+    done(null, user);
+  });
 });
 
 // ROUTES
@@ -100,6 +102,43 @@ app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
 });
+
+app.get('/account', ensureAuthenticated, function(req, res) {
+  // console.log(req.user);
+  var locals = {
+    user: req.user
+  };
+  res.render('account/show', locals);
+});
+
+app.get('/account/:id/edit', ensureAuthenticated, function(req, res) {
+  var locals = {
+    user: req.user
+  };
+  res.render('account/edit', locals);
+});
+
+
+app.put('/account/:id', ensureAuthenticated, function(req, res) {
+  User.update({
+    "_id": req.params.id
+  }, {
+    "username": req.body.username,
+    "password": req.body.password,
+    "email": req.body.email,
+    "first_name": req.body.first_name,
+    "last_name": req.body.last_name
+  }, function(err) {
+    if (err) {
+      throw err;
+    }
+    else {
+      res.redirect('/account');
+    }
+  });
+});
+
+
 
 app.get('/', function(req, res) {
   NewsItem.find(function(err, news) {
