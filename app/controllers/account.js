6a056508ca12877feb0b26513fa15c4ece33dc06
jpbatz <jph@hawaii.edu>
2015-01-15 17:22:08 -1000

    MBA refactors news.js controller, with errors

--- app/controllers/account.js
+++ app/controllers/account.js
@@ -1,4 +1,4 @@
-var User = require('./models/user');
+var User = require('../models/user');
 
 exports.show = function(req, res) {
   // console.log(req.user);
--- app/controllers/news.js
+++ app/controllers/news.js
@@ -1,7 +1,6 @@
-// TODO
-var NewsItem = require('./models/news');
+var NewsItem = require('../models/news');
 
-app.get('/news/:id', function(req, res) {
+exports.showItem = function(req, res) {
   NewsItem.find({
     "_id": req.params.id
   }, function(err, news) {
@@ -15,13 +14,33 @@ app.get('/news/:id', function(req, res) {
       res.render('./news', locals);
     }
   });
-});
+};
 
-app.get('/new_news', ensureAuthenticated, function(req, res) {
+// app.get('/news/:id', function(req, res) {
+//   NewsItem.find({
+//     "_id": req.params.id
+//   }, function(err, news) {
+//     if (err) {
+//       throw err;
+//     }
+//     else {
+//       var locals = {
+//         newsItem: news[0]
+//       };
+//       res.render('./news', locals);
+//     }
+//   });
+// });
+
+exports.showNew = function(req, res) {
   res.render('./new_news');
-});
+};
 
-app.post('/news', ensureAuthenticated, function(req, res) {
+// app.get('/new_news', ensureAuthenticated, function(req, res) {
+//   res.render('./new_news');
+// });
+
+exports.addNew = function(req, res) {
   var news = NewsItem({
     "title": req.body.title,
     "author": req.body.author,
@@ -36,9 +55,26 @@ app.post('/news', ensureAuthenticated, function(req, res) {
       res.redirect('/'); // Can't pass locals in to redirect
     }
   });
-});
+};
 
-app.get('/news/:id/edit', ensureAuthenticated, function(req, res) {
+// app.post('/news', ensureAuthenticated, function(req, res) {
+//   var news = NewsItem({
+//     "title": req.body.title,
+//     "author": req.body.author,
+//     "body": req.body.body
+//   });
+
+//   news.save(function(err) {
+//     if (err) {
+//       throw err;
+//     }
+//     else {
+//       res.redirect('/'); // Can't pass locals in to redirect
+//     }
+//   });
+// });
+
+exports.editGetItem = function(req, res) {
   NewsItem.find({
     "_id": req.params.id
   }, function(err, news) {
@@ -52,9 +88,25 @@ app.get('/news/:id/edit', ensureAuthenticated, function(req, res) {
       res.render('./edit_news', locals);
     }
   });
-});
+};
 
-app.put('/news/:id', ensureAuthenticated, function(req, res) {
+// app.get('/news/:id/edit', ensureAuthenticated, function(req, res) {
+//   NewsItem.find({
+//     "_id": req.params.id
+//   }, function(err, news) {
+//     if (err) {
+//       throw err;
+//     }
+//     else {
+//       var locals = {
+//         newsItem: news[0]
+//       };
+//       res.render('./edit_news', locals);
+//     }
+//   });
+// });
+
+exports.updateItem = function(req, res) {
   NewsItem.update({
     "_id": req.params.id
   }, {
@@ -69,9 +121,26 @@ app.put('/news/:id', ensureAuthenticated, function(req, res) {
       res.redirect('/news/' + req.params.id);
     }
   });
-});
+};
 
-app.delete('/news/:id', ensureAuthenticated, function(req, res) {
+// app.put('/news/:id', ensureAuthenticated, function(req, res) {
+//   NewsItem.update({
+//     "_id": req.params.id
+//   }, {
+//     "title": req.body.title,
+//     "author": req.body.author,
+//     "body": req.body.body
+//   }, function(err) {
+//     if (err) {
+//       throw err;
+//     }
+//     else {
+//       res.redirect('/news/' + req.params.id);
+//     }
+//   });
+// });
+
+exports.deleteItem = function(req, res) {
   NewsItem.remove({
     "_id": req.params.id
   }, function(err) {
@@ -82,4 +151,17 @@ app.delete('/news/:id', ensureAuthenticated, function(req, res) {
       res.redirect('/');
     }
   });
-});
+};
+
+// app.delete('/news/:id', ensureAuthenticated, function(req, res) {
+//   NewsItem.remove({
+//     "_id": req.params.id
+//   }, function(err) {
+//     if (err) {
+//       throw err;
+//     }
+//     else {
+//       res.redirect('/');
+//     }
+//   });
+// });
